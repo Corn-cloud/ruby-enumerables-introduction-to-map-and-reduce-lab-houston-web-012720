533fb7a84adf2c7e0739ca142c3138ae0561d70d
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-22 21:55:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,7 +29,15 @@ def map_to_double(source_array)
   return new 
 end 
 
-
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] )
+    i += 1
+  end
+  return new
+end
 
 def reduce_to_total(source_array, starting_point)
   i = 0 
